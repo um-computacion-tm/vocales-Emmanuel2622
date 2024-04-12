90001c53368b604dcb43bb962bcde2d2251a951b
Emmanuel <emmaden@localhost.localdomain>
2024-04-12 08:29:38 -0300

    Creacio de funcion cont_vocales

--- Vocales.py
+++ Vocales.py
@@ -1,8 +1,10 @@
+from unidecode import unidecode
 def cont_vocales(string):
     vocales = ('a', 'e', 'i', 'o', 'u')
     resultado = {}
+    string = unidecode(string)
     string_lower = string.lower()
-
+    
     for letra in string_lower:
         if letra in vocales:
             if letra not in resultado:
