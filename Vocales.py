24a663a12aa62701f3ed12663aa3b613b3d23afb
Emmanuel <emmaden@localhost.localdomain>
2024-04-12 09:18:49 -0300

    Upgrade, se remplazo unidecode por unicodedata

--- Vocales.py
+++ Vocales.py
@@ -1,10 +1,13 @@
-from unidecode import unidecode
+import unicodedata
+
 def cont_vocales(string):
     vocales = ('a', 'e', 'i', 'o', 'u')
     resultado = {}
-    string = unidecode(string)
+    string = unicodedata.normalize('NFKD', string)
+    string = ''.join([c for c in string if not unicodedata.combining(c)])
     string_lower = string.lower()
     
+    print(string)
     for letra in string_lower:
         if letra in vocales:
             if letra not in resultado:
